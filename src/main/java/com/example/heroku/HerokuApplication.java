fbe902dc1ccd9f82707ca75dbc63bb90c4a8d262
bensmus <bensmus@gmail.com>
2021-06-20 11:02:13 -0700

    Able to add and remove from table!

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -41,135 +41,137 @@ import java.util.Map;
 @SpringBootApplication
 public class HerokuApplication {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
 
-  @Autowired
-  private DataSource dataSource;
+	@Autowired
+	private DataSource dataSource;
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(HerokuApplication.class, args);
-  }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(HerokuApplication.class, args);
+	}
 
-  @GetMapping("/")
-  String index(Map<String, Object> model) { // you can add the thymeleaf thingy, in fact, its a must if you want dynamic webpage
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      String query = "SELECT * FROM rectangles";
-      ResultSet rs = stmt.executeQuery(query);
+	@GetMapping("/")
+	String index(Map<String, Object> model) { // you can add the thymeleaf thingy, in fact, its a must if you want dynamic webpage
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			String query = "SELECT * FROM rectangles";
+			ResultSet rs = stmt.executeQuery(query);
 
-      // loop through result set, making a list of Rectangle objects
-      ArrayList<Rectangle> rectangles = new ArrayList<>();
-      while (rs.next()) {
-        Rectangle rect = new Rectangle();
-        rect.setId(rs.getInt("id"));
-        rect.setName(rs.getString("name"));
-        rect.setColor(rs.getString("color"));
-        rect.setWidth(rs.getInt("width"));
-        rect.setHeight(rs.getInt("height"));
-        rectangles.add(rect);
-      }
+			// loop through result set, making a list of Rectangle objects
+			ArrayList<Rectangle> rects = new ArrayList<>();
+			while (rs.next()) {
+				Rectangle rect = new Rectangle();
+				rect.setId(rs.getInt("id"));
+				rect.setName(rs.getString("name"));
+				rect.setColor(rs.getString("color"));
+				rect.setWidth(rs.getInt("width"));
+				rect.setHeight(rs.getInt("height"));
+				rects.add(rect);
+			}
 
-      // display contents of db in HTML table
-      model.put("rectangles", rectangles);
+			// display contents of db in HTML table
+			model.put("rects", rects);
 
-      // debug af
-      for (Rectangle rectangle : rectangles) {
-        System.out.println(rectangle);
-      }
+			// debug af
+			for (Rectangle rect : rects) {
+				System.out.println(rect);
+			}
 
-      // have a blank rectangle ready for post requests
-      Rectangle rectangle = new Rectangle();
-      model.put("rectangle", rectangle);
-      return "index";
-    }
+			// have a blank rectangle ready for post requests
+			Rectangle rectangle = new Rectangle();
+			model.put("rectangle", rectangle);
+			return "index";
+		}
 
-    catch (SQLException e) {
-      System.out.println("Connection failed");
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
+		catch (SQLException e) {
+			System.out.println("Connection failed");
+			model.put("message", e.getMessage());
+			return "error";
+		}
+	}
 
-  @PostMapping("/submit") // triggered by submit button on form
-  String handleSubmit(Map<String, Object> model, Rectangle rect) {
-    System.out.println("Post request detected");
-    try (Connection connection = dataSource.getConnection()) {
-      System.out.println("Connection succeeded");
-      String name = rect.getName();
-      String color = rect.getColor();
-      Integer width = rect.getWidth();
-      Integer height = rect.getHeight();
-      String values = String.format("('%s','%s', '%d', '%d')", name, color, width, height);
-      System.out.println("Submitting rectangle " + values);
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate(
-          "CREATE TABLE IF NOT EXISTS rectangles (id SERIAL, name TEXT, color TEXT, width INTEGER, height INTEGER)");
-      String addRect = "INSERT INTO rectangles (name, color, width, height) VALUES " + values;
-      stmt.executeUpdate(addRect);
-      return "redirect:/"; // so that we run all of the code in @GetMapping("/")
+	@PostMapping("/submit") // triggered by submit button on form
+	String handleSubmit(Map<String, Object> model, Rectangle rect) {
+		System.out.println("Post request detected");
+		try (Connection connection = dataSource.getConnection()) {
+			System.out.println("Connection succeeded");
+			String name = rect.getName();
+			String color = rect.getColor();
+			Integer width = rect.getWidth();
+			Integer height = rect.getHeight();
+			String values = String.format("('%s','%s', '%d', '%d')", name, color, width, height);
+			System.out.println("Submitting rectangle " + values);
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate(
+					"CREATE TABLE IF NOT EXISTS rectangles (id SERIAL, name TEXT, color TEXT, width INTEGER, height INTEGER)");
+			String addRect = "INSERT INTO rectangles (name, color, width, height) VALUES " + values;
+			stmt.executeUpdate(addRect);
+			return "redirect:/"; // so that we run all of the code in @GetMapping("/")
 
-    } catch (SQLException e) {
-      System.out.println("Connection failed");
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
+		} catch (SQLException e) {
+			System.out.println("Connection failed");
+			model.put("message", e.getMessage());
+			return "error";
+		}
+	}
 
-  // Delete specific rectangle using button-> rectangle id is path variable
-  @DeleteMapping("/delrect/{id}")
-  String deleteRectId(Map<String, Object> model, @PathVariable String id) {
+	// Delete specific rectangle using button-> rectangle id is path variable
+	@DeleteMapping("/delrect/{id}")
+	String deleteRectId(Map<String, Object> model, @PathVariable String id) {
 
-    System.out.println("Delete request detected");
-    try (Connection connection = dataSource.getConnection()) {
-      System.out.println("Connection succeeded");
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate(String.format("DELETE FROM rectangles WHERE id=%s", id));
-      return "redirect:/"; // we go to a path well traveled ;)
+		System.out.println("Delete request detected");
+		try (Connection connection = dataSource.getConnection()) {
+			System.out.println("Connection succeeded");
+			System.out.printf("ID=%s\n", id);
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate(String.format("DELETE FROM rectangles WHERE id=%s", id));
+			return "redirect:/"; // we go to a path well traveled ;)
 
-    } catch (Exception e) {
-      System.out.println("Connection failed");
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
+		} catch (Exception e) {
+			System.out.println("Connection failed");
+			e.printStackTrace();
+			model.put("message", e.getMessage());
+			return "error";
+		}
+	}
 
-  @GetMapping("/db_time")
-  String db_time(Map<String, Object> model) { // function can be called anything
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+	@GetMapping("/db_time")
+	String db_time(Map<String, Object> model) { // function can be called anything
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
+			ArrayList<String> output = new ArrayList<String>();
+			while (rs.next()) {
+				output.add("Read from DB: " + rs.getTimestamp("tick"));
+			}
 
-      model.put("records", output); // this is for thymeleaf
-      return "db_time";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
+			model.put("records", output); // this is for thymeleaf
+			return "db_time";
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "error";
+		}
+	}
 
-  @GetMapping("/rectangle")
-  String rectangle(Map<String, Object> model) {
-    return "rectangle";
-  }
+	@GetMapping("/rectangle")
+	String rectangle(Map<String, Object> model) {
+		return "rectangle";
+	}
 
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      System.out.println("CUSTOM OUTPUT-- Database URL: " + dbUrl);
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			System.out.println("CUSTOM OUTPUT-- Database URL: " + dbUrl);
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
 
 }
